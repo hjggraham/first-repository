478f5a35604a0321d3e49193e5cfd32b96f8e2d5
hjggraham <hamish@graham.gb.net>
2021-04-29 09:56:21 +0000

    code cleaned up, final equation modified

--- alfa.py
+++ alfa.py
@@ -1,4 +1,4 @@
-def checkSimp(matrix):
+def checksimp(matrix):
     '''This is to check wether a matrix is simplectic'''
     my_dict = {}
     #imput has to be in np.array()
@@ -9,14 +9,15 @@ def checkSimp(matrix):
     else:
         my_dict['is_square'] = False
     #creating omega matrix
-    identity_n = my_dict['dim_a']/2
-    identity_m = np.identity(int(identity_n))
-    zeros = np.zeros((int(identity_n), int(identity_n)))
+    identity_n = int(my_dict['dim_a']/2)
+    identity_m = np.identity(identity_n)
+    zeros = np.zeros((identity_n, identity_n))
     my_dict['omega'] = np.block([[zeros, identity_m], 
                                  [-identity_m, zeros]])
     #final check for symplectic matrix
-    r_side = matrix.T@my_dict['omega']@matrix
-    if (my_dict['omega'] == r_side).all():
+    precision = 10 ** -12
+    l_side = matrix.T @ my_dict['omega'] @ matrix
+    if (l_side - my_dict['omega'] < precision).all():
         my_dict['is_simplectic'] = True
     else:
         my_dict['is_simplectic'] = False
