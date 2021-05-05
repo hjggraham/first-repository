6a07fae4ccea9a04945be1d63fc8340cc6c4fd48
hjggraham <83212958+hjggraham@users.noreply.github.com>
2021-05-05 16:46:05 +0200

    Update alfa.py

--- alfa.py
+++ alfa.py
@@ -2,7 +2,38 @@ import numpy as np
 
 
 def check_symplecticity(matrix, precision=10 ** -12):
-    '''This is to check wether a matrix is simplectic.'''
+    '''This is to check wether a matrix is simplectic.
+     
+    Parameters
+    ----------
+    in_value
+        A variable of any type that we want to check is a number.
+    Returns
+    -------
+    bool
+        True/False depending on whether it was a number.
+    Examples
+    --------
+    >>> is_number(1)
+    True
+    >>> is_number(1.0)
+    True
+    >>> is_number("1")
+    True
+    >>> is_number("1.0")
+    True
+    >>> is_number("Hello")
+    False
+    You can also pass more complex objects, these will all be ``False``.
+    >>> is_number({"hello": "world"})
+    False
+    >>> from datetime import datetime
+    >>> is_number(datetime.now())
+    False
+    Even something which contains all numbers will be ``False``, because it is not itself a number.
+    >>> is_number([1, 2, 3, 4])
+    False'''
+
     my_dict = {}
     my_dict['is_square'] = False
     my_dict['is_symplectic'] = False
