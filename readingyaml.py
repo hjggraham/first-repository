eb042db19526b6505a23e6b162a1bdfb77f85e73
hjggraham <83212958+hjggraham@users.noreply.github.com>
2021-05-11 20:17:21 +0200

    Update readingyaml.py

--- readingyaml.py
+++ readingyaml.py
@@ -1,27 +1,7 @@
 
 import ruamel.yaml
 import datetime
-"""
-read_yaml is to read a yaml file and convert it into python.
-write_yaml is to convert a dictionary into a yaml file.
-append_yaml is to append dictionaries to a yaml file.
-time_yaml is to create timestamps and add them to a yaml file.
 
-Examples
---------
->>> read_yaml('mytest')
-# reads 'mytest' and converts it to a dictionary in python
-
->>> write_yaml({'green': 'hello'}, ('mytest2.yml'))
-# converts a dictionary into yaml file ('mytest2.yml').
-
->>> append_yaml({'blue': 'bonjour'}, ('mytest2.yml'))
-# appends the new dictionary to the existing ('mytest2.yml') file.
-
->>> time_yaml('myfile', 'hello', 'first stage')
-# creates a time stamp with a comment, "hello", under the dictionary "first stage" in "myfile"
-
-"""
 # load the configuration
 def read_yaml(myfile):
 	with open(myfile, 'r') as file:
@@ -61,6 +41,27 @@ def get_last_stage(myfile):
 
 
 def tag1_it(myfile, mycomment):
+	"""
+	read_yaml is to read a yaml file and convert it into python.
+	write_yaml is to convert a dictionary into a yaml file.
+	append_yaml is to append dictionaries to a yaml file.
+	time_yaml is to create timestamps and add them to a yaml file.
+
+	Examples
+	--------
+	>>> read_yaml('mytest')
+	# reads 'mytest' and converts it to a dictionary in python
+
+	>>> write_yaml({'green': 'hello'}, ('mytest2.yml'))
+	# converts a dictionary into yaml file ('mytest2.yml').
+
+	>>> append_yaml({'blue': 'bonjour'}, ('mytest2.yml'))
+	# appends the new dictionary to the existing ('mytest2.yml') file.
+
+	>>> time_yaml('myfile', 'hello', 'first stage')
+	# creates a time stamp with a comment, "hello", under the dictionary "first stage" in "myfile"
+
+	"""	
 	stage = get_last_stage(myfile)
 	with open(myfile, 'a') as file:
 		yaml = ruamel.yaml.YAML() 
