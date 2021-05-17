24b27c281838444ed1df90d467f4ebc0cf83e949
hjggraham <hamish@graham.gb.net>
2021-05-17 15:06:20 +0200

    removed the import yaml

--- test_readingyaml.py
+++ test_readingyaml.py
@@ -1,10 +1,9 @@
 import readingyaml as ry
 import numpy as np
 import pytest
-import yaml
 import datetime
-import ruamel
-import ordereddict
+import ruamel.yaml
+from collections import OrderedDict
 
 def test_exeption():
     with pytest.raises(Exception):
@@ -17,9 +16,9 @@ with open('hamishtest.yml', 'r') as f:
 
 
 def test_valid_number():
-    assert test_data == ordereddict([('first', ordereddict([('hr', datetime.datetime(2021, 5, 11, 16, 50, 39, 84511)), ('unix', 1620751839.08453), ('mycomment', 'hello')]))])
+    assert test_data == OrderedDict([('first', OrderedDict([('hr', datetime.datetime(2021, 5, 11, 16, 50, 39, 84511)), ('unix', 1620751839.08453), ('mycomment', 'hello')]))])
 
 def test_append():
     ry.append_yaml({'blue': 'bonjour'}, ('hamishtest.yml'))
-    assert ry.read_yaml('hamishtest.yml') == ordereddict([('first', ordereddict([('hr', datetime.datetime(2021, 5, 11, 16, 50, 39, 84511)), ('unix', 1620751839.08453), ('mycomment', 'hello')])), ('blue', 'bonjour')])
+    assert ry.read_yaml('hamishtest.yml') == OrderedDict([('first', OrderedDict([('hr', datetime.datetime(2021, 5, 11, 16, 50, 39, 84511)), ('unix', 1620751839.08453), ('mycomment', 'hello')])), ('blue', 'bonjour')])
 
